7fb5a8787fbc2c8e9561f58a8b90e317635f9e21
unknown <cagdas@.(none)>
2012-11-28 11:12:06 -0500

    all

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,28 @@
+class SubjectsController < ApplicationController
+  
+  def index
+      list
+      render('list')
+  end
+  
+  def list
+    @subjects = Subject.order("subjects.position ASC")
+  end
+  
+  def show
+    @subject = Subject.find(params[:id])
+  end
+  
+  def new
+    @subject = Subject.new(:name => 'default')
+  end
+  
+  def create
+    @subject = Subject.new(params[:subject])
+    if @subject.save
+      redirect_to(:action => 'list')
+    else
+      render(')
+    end
+  end
+end
